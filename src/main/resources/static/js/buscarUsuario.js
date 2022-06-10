9c039cbfa745bcfe698a8b2ae8f633b7a12eb07a
EloyMaldonadoC <eloy.maldonado@uaz.edu.mx>
2022-06-10 10:50:22 -0500

    commit version 1.2.4

--- src/main/resources/static/js/buscarUsuario.js
+++ src/main/resources/static/js/buscarUsuario.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 //Busca un usuario a partir de una cadena dada
 async function buscarUsuario(){
     //redirecciona
-    window.location.href = 'Busqueda.html'
+    var url = "Busqueda.html";
+    $(location).attr('href',url);
     let cade = document.getElementById('buscar').value;
     localStorage.busqueda = cade;
     alert(cade);
